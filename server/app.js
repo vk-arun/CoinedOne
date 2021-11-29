43dec67bf53dbdf2cf95faca6bdf67c4f0f70941
vk-arun <arunvkmonu@gmail.com>
2021-11-29 12:16:29 +0530

    git commit

--- server/app.js
+++ server/app.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const app = express();
-const connectDb = require('./config/conn');
-const appRoutes = require('./routes/schedules');
+const connectDb = require("./config/conn");
+const appRoutes = require("./routes/schedules");
 
 connectDb();
-app.use(express.json({extended: false}));
-app.use(express.urlencoded({extended: true}));
+app.use(express.json({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/apps', appRoutes );
+app.use("/api/apps", appRoutes);
 
+console.log("change");
 
 var PORT = process.env.PORT || 2000;
 app.listen(PORT, () => {
-    console.log(` app is running on port ${ PORT }`);
+  console.log(` app is running on port ${PORT}`);
 });
-
